3502f8a5acd94bc4c4a857265845880e0e774a53
Hieu Le <lexuanhieu.bme@gmail.com>
2020-06-28 01:01:00 +0700

    fix logging

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ import ssl
 import os
 from datetime import datetime
 
+
 def main():
     # read configure file
     with open("cfgs/tenes.cfg") as f:
@@ -53,12 +54,12 @@ def main():
     extractor_name = cfg["train"]["extractor"]
     model = cls.ClassificationModel(model_name=extractor_name).create_model()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    logging.info("Using device: {} ".format( device))
+    logging.info("Using device: {} ".format(device))
     # convert to suitable device
     # global model
     model = model.to(device)
     logging.info("Model created...")
-    
+
     # create a metric for evaluating
     # global train_metrics
     # global val_metrics
@@ -76,13 +77,15 @@ def main():
     try:
         # if the loss function comes from nn package
         criterion = getattr(
-        nn, loss_function, "The loss {} is not available".format(loss_function)
-    )
+            nn, loss_function, "The loss {} is not available".format(loss_function)
+        )
     except:
         # use custom loss
         criterion = getattr(
-        custom_loss, loss_function, "The loss {} is not available".format(loss_function)
-    )
+            custom_loss,
+            loss_function,
+            "The loss {} is not available".format(loss_function),
+        )
     criterion = criterion()
     optimizer = getattr(
         torch.optim, optimizers, "The optimizer {} is not available".format(optimizers)
@@ -97,20 +100,18 @@ def main():
 
     # before training, let's create a file for logging model result
     time_str = str(datetime.now().strftime("%Y%m%d-%H%M"))
-    log_file = logger.make_file(cfg["session"]["sess_name"],time_str)
+    log_file = logger.make_file(cfg["session"]["sess_name"], time_str)
     logger.log_initilize(log_file)
     print("Beginning training...")
     # export the result to log file
-    f = open("cfgs/tenes.cfg","w+")
+    f = open("cfgs/tenes.cfg", "w+")
     logging.info("-----")
     logging.info("session name: {} \n".format(cfg["session"]["sess_name"]))
     logging.info(model)
     logging.info("\n")
-    logging.info("CONFIGS")
+    logging.info("CONFIGS \n")
     # logging the configs:
-    for line in f:
-        logging.info(line)
-        logging.info("\n")
+    logging.info(f.read())
     # training models
     num_epoch = int(cfg["train"]["num_epoch"])
     best_val_acc = 0
@@ -140,12 +141,21 @@ def main():
         logging.info(val_result)
         logging.info("\n")
         # saving epoch with best validation accuracy
-        if (best_val_acc < float (val_result["accuracy_score"])):
-            logging.info("Validation accuracy= ",val_result["accuracy_score"], "===> Save best epoch")
+        if best_val_acc < float(val_result["accuracy_score"]):
+            logging.info(
+                "Validation accuracy= ",
+                val_result["accuracy_score"],
+                "===> Save best epoch",
+            )
             best_val_acc = val_result["accuracy_score"]
-            torch.save(model.state_dict(), "saved/models/" +  time_str+'-'+cfg["train"]["save_as_name"])
+            torch.save(
+                model.state_dict(),
+                "saved/models/" + time_str + "-" + cfg["train"]["save_as_name"],
+            )
         else:
-            logging.info("Validation accuracy= ",val_result["accuracy_score"], "===> No saving")
+            logging.info(
+                "Validation accuracy= ", val_result["accuracy_score"], "===> No saving"
+            )
             continue
 
     # testing on test set
@@ -162,10 +172,12 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     test_loader = torch.utils.data.DataLoader(testing_set, batch_size=1, shuffle=False)
     print("Inference on the testing set")
-    
+
     # load the test model and making inference
     test_model = cls.ClassificationModel(model_name=extractor_name).create_model()
-    model_path = os.path.join("saved/models", time_str+'-'+cfg["train"]["save_as_name"])
+    model_path = os.path.join(
+        "saved/models", time_str + "-" + cfg["train"]["save_as_name"]
+    )
     test_model.load_state_dict(torch.load(model_path))
     test_model = test_model.to(device)
     logging.info(tester.test_result(test_model, test_loader, device))
